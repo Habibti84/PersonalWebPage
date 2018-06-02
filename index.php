1abfcea24167f8a651d26d0e75cf5e66dcd1656f
Jacqueline Motzer <jacmotzer@gmail.com>
2018-06-02 21:05:55 +0200

    1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/main.css">
+    <title>Home</title>
+
+
+
+  <body>
+
+
+
+    <div class="container">
+      <img src="pics/Logo.jpg" alt="">
+    </div>
+
+
+
+
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Angebot</a></li>
+        <li><a href="#">Über mich</a></li>
+        <li><a href="#">Kontakt</a></li>
+        <li><a href="#"><i class="fas fa-user"></i></span>
+        </a></li>
+      </ul>
+    </nav>
+
+
+
+
+  </body>
+</html>
